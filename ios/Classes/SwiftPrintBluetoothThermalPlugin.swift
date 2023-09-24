54b6ee20943807e2a12d92b75596ab4b60cc187e
Tristan Harvey Godoy <104707216+popodepo123@users.noreply.github.com>
2023-09-24 22:53:22 +0800

    Update SwiftPrintBluetoothThermalPlugin.swift

--- ios/Classes/SwiftPrintBluetoothThermalPlugin.swift
+++ ios/Classes/SwiftPrintBluetoothThermalPlugin.swift
@@ -93,7 +93,7 @@ public class SwiftPrintBluetoothThermalPlugin: NSObject, CBCentralManagerDelegat
       }
 
         // despues de 5 segundos se para la busqueda y se devuelve la lista de dispositivos disponibles
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 15) {
             self.centralManager?.stopScan()
             print("Stopped scanning -> Discovered devices: \(self.discoveredDevices.count)")
             result(self.discoveredDevices)
